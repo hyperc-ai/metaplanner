e6a9a1db4e34a32aee3d5e7f0cf1b99f486747c1
Andrew Gree <andrew@hyperc.com>
2021-02-10 16:56:29 -0800

    injected basic settings to metaplanner main

--- metaplanner/__main__.py
+++ metaplanner/__main__.py
@@ -5,6 +5,18 @@ import sys
 from metaplanner.planner import Predicate, Action, Problem, Domain, Object, PredicateId, Parameter
 import metaplanner.pddl
 from hyperc import solve
+import hyperc
+hyperc.settings.STORE_STDOUT=1 
+hyperc.settings.STORE_SAS=1 
+hyperc.settings.HYPERC_SPLIT_OFF=1 
+hyperc.settings.HYPERC_SOLVER_MAX_TIME=6000 
+hyperc.settings.HYPERC_LOGLEVEL="info" 
+hyperc.settings.HYPERC_SOLVER_LOGLEVEL="info" 
+hyperc.settings.DOWNWARD_TOTAL_PUSHES=5000000000 
+hyperc.settings.HYPERC_STRICT_TYPING=1 
+hyperc.settings.HYPERC_LIN_COUNT=9 
+hyperc.settings.HYPERC_NEW_OBJECTS=7 
+hyperc.settings.HYPERC_ASE_OFF=1
 
 if __name__ == "__main__":
     domain, problem, predicate_factory, object_factory, parameter_factories, action_factory = \
