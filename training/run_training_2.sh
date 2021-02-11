83a4bc0e3709b6a73395aeb13dc239209fe35c1e
Andrew Gree <andrew@hyperc.com>
2021-02-11 11:04:13 -0800

    fixed training folders

--- training/run_training_2.sh
+++ training/run_training_2.sh
@@ -2,5 +2,5 @@
 
 for i in `seq 0 200`; do
     python ./training/random_pddl_generator.py 1 3 ${i} ./training/$1/ &&
-    HYPERC_TEMPDIR=/tmp/hyperc/training/$1 HYPERC_STORE_STDOUT=1 HYPERC_STORE_SAS=1 HYPERC_SPLIT_OFF=1 HYPERC_SOLVER_MAX_TIME=6000 HYPERC_LOGLEVEL=info HYPERC_SOLVER_LOGLEVEL=info DOWNWARD_TOTAL_PUSHES=5000000000 HYPERC_STRICT_TYPING=1 python -m metaplanner training/$1/domain_${i}.pddl training/$1/problem_${i}.pddl
+    HYPERC_TEMPDIR=/var/data/userdata/training/$1 HYPERC_STORE_STDOUT=1 HYPERC_STORE_SAS=1 HYPERC_SPLIT_OFF=1 HYPERC_SOLVER_MAX_TIME=6000 HYPERC_LOGLEVEL=info HYPERC_SOLVER_LOGLEVEL=info DOWNWARD_TOTAL_PUSHES=5000000000 HYPERC_STRICT_TYPING=1 python -m metaplanner training/$1/domain_${i}.pddl training/$1/problem_${i}.pddl
 done
