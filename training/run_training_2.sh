455f3f51f82ccc97a18f4ec383b02216093ce4f8
Andrew Gree <andrew@hyperc.com>
2021-02-10 22:37:51 -0800

    fix training script

--- training/run_training_2.sh
+++ training/run_training_2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 for i in `seq 0 200`; do
-    # HYPERC_TEMPDIR=/tmp/hyperc/training/$1 HYPERC_USE_CACHE=1 HYPERC_STORE_STDOUT=1 HYPERC_STORE_SAS=1 HYPERC_SPLIT_OFF=1 HYPERC_SOLVER_MAX_TIME=6000 HYPERC_LOGLEVEL=info HYPERC_SOLVER_LOGLEVEL=info DOWNWARD_TOTAL_PUSHES=5000000000 HYPERC_STRICT_TYPING=1 python ./metaplanner.py training/$1/domain_${i}.pddl training/$1/problem_${i}.pddl
-    python ./random_pddl_generator.py 1 3 ${i} ./training/$1/ &&
-    HYPERC_TEMPDIR=/tmp/hyperc/training/$1 HYPERC_STORE_STDOUT=1 HYPERC_STORE_SAS=1 HYPERC_SPLIT_OFF=1 HYPERC_SOLVER_MAX_TIME=6000 HYPERC_LOGLEVEL=info HYPERC_SOLVER_LOGLEVEL=info DOWNWARD_TOTAL_PUSHES=5000000000 HYPERC_STRICT_TYPING=1 python ./metaplanner.py training/$1/domain_${i}.pddl training/$1/problem_${i}.pddl
+    python ./training/random_pddl_generator.py 1 3 ${i} ./training/$1/ &&
+    HYPERC_TEMPDIR=/tmp/hyperc/training/$1 HYPERC_STORE_STDOUT=1 HYPERC_STORE_SAS=1 HYPERC_SPLIT_OFF=1 HYPERC_SOLVER_MAX_TIME=6000 HYPERC_LOGLEVEL=info HYPERC_SOLVER_LOGLEVEL=info DOWNWARD_TOTAL_PUSHES=5000000000 HYPERC_STRICT_TYPING=1 python -m metaplanner training/$1/domain_${i}.pddl training/$1/problem_${i}.pddl
 done
