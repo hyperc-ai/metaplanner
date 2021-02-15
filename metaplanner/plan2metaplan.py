3ce18734fc23b2cdf3194d52ad1ce9af1e12659e
Andrew Gree <andrew@hyperc.com>
2021-02-15 14:06:50 -0800

    fixed hinting of constants

--- metaplanner/plan2metaplan.py
+++ metaplanner/plan2metaplan.py
@@ -27,60 +27,120 @@ def load_hints(splan, action_factory: pd.ActionFactory, parameter_factories: typ
         #   which object from its places
         # (action-name obj1 obj2 obj3)
 
+        assert len(list(pf.parameters.keys())) == len(parameters_objects)
+
         par_pddl_ids = list(zip(pf.parameters.keys(), parameters_objects))
-        # print("Hints for line", l)
-        # print(par_pddl_ids)
+        print("Hints for line", l, par_pddl_ids)
         action_obj = action_factory.name_action_map[action_name]
         hint_cnt = 0
         for parname, objid in par_pddl_ids:
             for precond in action_factory.name_action_map[action_name].precondition:
+                pred_obj = precond
+                par_obj = object_factory.objects[objid]
+                # precondition_hints["pred-obj1"].append((plan_len, action_obj, pred_obj, par_obj))
+                # precondition_hints["pred-obj2"].append((plan_len, action_obj, pred_obj, par_obj))
+                # precondition_hints["pred-obj1-cln"].append((action_obj, pred_obj, par_obj))
+                # precondition_hints["pred-obj1-cln"].append((action_obj, pred_obj, par_obj))
+                # precondition_hints["pred-obj1"].append((plan_len, action_obj, pred_obj, metaplanner.planner.OBJ_NONE))
+                # precondition_hints["pred-obj2"].append((plan_len, action_obj, pred_obj, metaplanner.planner.OBJ_NONE))
+                # precondition_hints["pred-obj1-cln"].append((action_obj, pred_obj, metaplanner.planner.OBJ_NONE))
+                # precondition_hints["pred-obj1-cln"].append((action_obj, pred_obj, metaplanner.planner.OBJ_NONE))
                 if precond.par_1 == parameter_factories[action_name].parameters[parname]:
-                    pred_obj = precond
-                    par_obj = object_factory.objects[objid]
-                    # TODO: add action object (when many actions)
-                    # TODO: add plan len, action
                     if pred_obj.name == metaplanner.planner.EQ_PREDICATE:
+                        assert pred_obj.par_1._class == par_obj._class
                         precondition_hints["pred-eq-obj1"].append((plan_len, action_obj, pred_obj, par_obj))
                         precondition_hints["pred-obj1-cln"].append((action_obj, pred_obj, par_obj))
                         precondition_hints["pred-obj1-cln"].append((action_obj, pred_obj, metaplanner.planner.OBJ_NONE))
                     else:
+                        assert pred_obj.par_1._class == par_obj._class
                         precondition_hints["pred-obj1"].append((plan_len, action_obj, pred_obj, par_obj))
                         precondition_hints["pred-obj1-cln"].append((action_obj, pred_obj, par_obj))
                         precondition_hints["pred-obj1-cln"].append((action_obj, pred_obj, metaplanner.planner.OBJ_NONE))
                     hint_cnt += 1
                 if precond.par_2 == parameter_factories[action_name].parameters[parname]:
-                    pred_obj = precond
-                    par_obj = object_factory.objects[objid]
                     if pred_obj.name == metaplanner.planner.EQ_PREDICATE:
+                        assert pred_obj.par_2._class == par_obj._class
                         precondition_hints["pred-eq-obj2"].append((plan_len, action_obj, pred_obj, par_obj))
                         precondition_hints["pred-obj2-cln"].append((action_obj, pred_obj, par_obj))
                         precondition_hints["pred-obj2-cln"].append((action_obj, pred_obj, metaplanner.planner.OBJ_NONE))
                     else:
+                        assert pred_obj.par_2._class == par_obj._class
                         precondition_hints["pred-obj2"].append((plan_len, action_obj, pred_obj, par_obj))
                         precondition_hints["pred-obj2-cln"].append((action_obj, pred_obj, par_obj))
                         precondition_hints["pred-obj2-cln"].append((action_obj, pred_obj, metaplanner.planner.OBJ_NONE))
                     hint_cnt += 1
             for precond in action_factory.name_action_map[action_name].effect:
                 print("HINTS checking effect", precond)
+                pred_obj = precond
+                par_obj = object_factory.objects[objid]
+                # effect_hints["eff-obj1"].append((plan_len, action_obj, pred_obj, par_obj))
+                # effect_hints["eff-obj2"].append((plan_len, action_obj, pred_obj, par_obj))
+                # effect_hints["eff-obj1-cln"].append((action_obj, pred_obj, par_obj))
+                # effect_hints["eff-obj2-cln"].append((action_obj, pred_obj, par_obj))
+                # effect_hints["eff-obj1"].append((plan_len, action_obj, pred_obj, metaplanner.planner.OBJ_NONE,))
+                # effect_hints["eff-obj2"].append((plan_len, action_obj, pred_obj, metaplanner.planner.OBJ_NONE,))
+                # effect_hints["eff-obj1-cln"].append((action_obj, pred_obj, metaplanner.planner.OBJ_NONE,))
+                # effect_hints["eff-obj2-cln"].append((action_obj, pred_obj, metaplanner.planner.OBJ_NONE,))
                 if precond.par_1 == parameter_factories[action_name].parameters[parname]:
                     print("HINTS found par 1 in effect", action_name, parname, "=", objid)
                     pred_obj = precond
                     par_obj = object_factory.objects[objid]
+                    assert pred_obj.par_1._class == par_obj._class
                     effect_hints["eff-obj1"].append((plan_len, action_obj, pred_obj, par_obj))
                     effect_hints["eff-obj1-cln"].append((action_obj, pred_obj, par_obj,))
                     effect_hints["eff-obj1-cln"].append((action_obj, pred_obj, metaplanner.planner.OBJ_NONE,))
-                    # TODO: add action object (when many actions)
                     hint_cnt += 1
                 if precond.par_2 == parameter_factories[action_name].parameters[parname]:
                     print("HINTS found par 2 in effect", action_name, parname, "=", objid)
                     pred_obj = precond
                     par_obj = object_factory.objects[objid]
+                    assert pred_obj.par_2._class == par_obj._class
                     effect_hints["eff-obj2"].append((plan_len, action_obj, pred_obj, par_obj))
                     effect_hints["eff-obj2-cln"].append((action_obj, pred_obj, par_obj,))
                     effect_hints["eff-obj2-cln"].append((action_obj, pred_obj, metaplanner.planner.OBJ_NONE,))
                     hint_cnt += 1
             # TODO: calculate which branches went with NONE objects for cleaning
-            par_obj = object_factory.objects[objid]
+        # TODO HERE: Now add all the static hints for this plan len
+        for precond in action_factory.name_action_map[action_name].precondition:
+            pred_obj = precond
+            if precond.par_1.const == True:
+                par_obj = precond.par_1.obj
+                if pred_obj.name == metaplanner.planner.EQ_PREDICATE:
+                    assert pred_obj.par_1._class == par_obj._class
+                    precondition_hints["pred-eq-obj1"].append((plan_len, action_obj, pred_obj, par_obj))
+                    precondition_hints["pred-obj1-cln"].append((action_obj, pred_obj, par_obj))
+                    precondition_hints["pred-obj1-cln"].append((action_obj, pred_obj, metaplanner.planner.OBJ_NONE))
+                else:
+                    assert pred_obj.par_1._class == par_obj._class
+                    precondition_hints["pred-obj1"].append((plan_len, action_obj, pred_obj, par_obj))
+                    precondition_hints["pred-obj1-cln"].append((action_obj, pred_obj, par_obj))
+                    precondition_hints["pred-obj1-cln"].append((action_obj, pred_obj, metaplanner.planner.OBJ_NONE))
+            if precond.par_2.const == True:
+                par_obj = precond.par_2.obj
+                if pred_obj.name == metaplanner.planner.EQ_PREDICATE:
+                    assert pred_obj.par_2._class == par_obj._class
+                    precondition_hints["pred-eq-obj2"].append((plan_len, action_obj, pred_obj, par_obj))
+                    precondition_hints["pred-obj2-cln"].append((action_obj, pred_obj, par_obj))
+                    precondition_hints["pred-obj2-cln"].append((action_obj, pred_obj, metaplanner.planner.OBJ_NONE))
+                else:
+                    assert pred_obj.par_2._class == par_obj._class
+                    precondition_hints["pred-obj2"].append((plan_len, action_obj, pred_obj, par_obj))
+                    precondition_hints["pred-obj2-cln"].append((action_obj, pred_obj, par_obj))
+                    precondition_hints["pred-obj2-cln"].append((action_obj, pred_obj, metaplanner.planner.OBJ_NONE))
+        for precond in action_factory.name_action_map[action_name].effect:
+            pred_obj = precond
+            if precond.par_1.const == True:
+                par_obj = precond.par_1.obj
+                assert pred_obj.par_1._class == par_obj._class
+                effect_hints["eff-obj1"].append((plan_len, action_obj, pred_obj, par_obj))
+                effect_hints["eff-obj1-cln"].append((action_obj, pred_obj, par_obj,))
+                effect_hints["eff-obj1-cln"].append((action_obj, pred_obj, metaplanner.planner.OBJ_NONE,))
+            if precond.par_2.const == True:
+                par_obj = precond.par_2.obj
+                assert pred_obj.par_2._class == par_obj._class
+                effect_hints["eff-obj2"].append((plan_len, action_obj, pred_obj, par_obj))
+                effect_hints["eff-obj2-cln"].append((action_obj, pred_obj, par_obj,))
+                effect_hints["eff-obj2-cln"].append((action_obj, pred_obj, metaplanner.planner.OBJ_NONE,))
         plan_len += 1
 
         # print(hints)
